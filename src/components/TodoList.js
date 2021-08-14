8ce053e2fd7d75568883c1ef7ba7a7f23860d94b
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-08-14 17:30:45 +0900

    merge

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 import { FiPlusCircle } from "react-icons/fi";
