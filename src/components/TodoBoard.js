98e7d1a19926fcc8b6d0b17f1ae96ca2646b35dc
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-08-14 22:14:35 +0900

    todo done
    need to do with the backend and more

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -3,10 +3,16 @@ import TodoList from "./TodoList";
 
 const TodoBoard = () => {
   // todoList -> todo 배열의 배열
+  // ['apple', 'avocado', 'ace'], ['banana','bun', 'bowl'], ['cake','cactus'] ... 이렇게 가야함
   const [todoLists, setTodoLists] = useState([[]]);
+  // const [isMother, setIsMother] = useState(true);
 
   // post it 추가
   const addPostit = () => {
+    // postit 하나만 있을 때
+    if (todoLists.length === 1) {
+    }
+
     const newTodoLists = [[], ...todoLists];
 
     setTodoLists(newTodoLists);
@@ -22,3 +28,10 @@ const TodoBoard = () => {
 };
 
 export default TodoBoard;
+
+// mother posts에서만 add post 되도록 구현해야함
+// backend 구현해야함 (MERN ? , Firebase? , 일단 Local Storage?)
+// Board 안에서 post it 드래그 앤 드랍 할 수 있도록 구현
+// 완성된 todo는 밑으로 보내서 해야할 것들이 todo list의 상단에 올라오도록
+// css post it 처럼 꾸미기
+// todo list 색 바꿀 수 있도록 수정 (오른쪽 마우스 클릭해서 몇 가지 색으로만 바꿀 수 있도록)
