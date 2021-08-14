f7594ef8aa354736ce1e82e0053ca820c4d92a4b
JISU-Y <80020227+JISU-Y@users.noreply.github.com>
2021-08-14 17:13:20 +0900

    update to do in each postit

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -3,30 +3,20 @@ import TodoList from "./TodoList";
 
 const TodoBoard = () => {
   // todoList -> todo 배열의 배열
-  const [todoLists, setTodoLists] = useState([
-    ["apple", "acorn"],
-    ["banana", "basement", "bamboo"],
-    ["case"],
-  ]);
+  const [todoLists, setTodoLists] = useState([[]]);
 
   // post it 추가
   const addPostit = () => {
-    const todoList = [];
-    const newTodoLists = [todoList, ...todoLists];
+    const newTodoLists = [[], ...todoLists];
 
     setTodoLists(newTodoLists);
   };
 
   return (
-    // <div className="todo-app">
-    // </div>
     <>
-      {/* <TodoList /> */}
       {todoLists.map((todos, index) => {
-        console.log(index);
-        return <TodoList key={index} todos={todos} />;
+        return <TodoList key={index} todos={todos} addPostit={addPostit} />;
       })}
-      <button onClick={addPostit}>add post it</button>
     </>
   );
 };
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 import { FiPlusCircle } from "react-icons/fi";
 
-// todos 배열 넘겨받음
-const TodoList = ({ todos }) => {
-  // const [todos, setTodos] = useState([]);
-
-  console.log(todos);
+const TodoList = ({ addPostit }) => {
+  // 여기서 따로 사용할 todo 배열
+  const [todoArr, setTodoArr] = useState([]);
 
   const addTodo = (todo) => {
     // 추가하려고 하는 todo의 text를 검사
@@ -16,21 +14,21 @@ const TodoList = ({ todos }) => {
     }
 
     // 새 todo와 기존의 todos 배열을 합침
-    const newTodos = [todo, ...todos];
+    const newTodos = [todo, ...todoArr];
 
     // Todos를 새로 만든 todos 배열로 set함
-    // setTodos(newTodos);
+    setTodoArr(newTodos);
   };
 
-  // const updateTodo = (todoId, newValue) => {
-  //   if (!newValue.text || /^\s*$/.test(newValue.text)) {
-  //     return;
-  //   }
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
 
-  //   setTodos((prev) =>
-  //     prev.map((item) => (item.id === todoId ? newValue : item))
-  //   );
-  // };
+    setTodoArr((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
 
   // const removeTodo = (id) => {
   //   const removeArr = [...todos].filter((todo) => todo.id !== id);
@@ -53,12 +51,12 @@ const TodoList = ({ todos }) => {
     <div className="todo-app">
       <TodoForm onSubmit={addTodo} />
       <Todo
-        todos={todos}
+        todos={todoArr}
         // completeTodo={completeTodo}
         // removeTodo={removeTodo}
-        // updateTodo={updateTodo}
+        updateTodo={updateTodo}
       />
-      <FiPlusCircle className="plus-icon" />
+      <FiPlusCircle className="plus-icon" onClick={addPostit} />
     </div>
   );
 };
